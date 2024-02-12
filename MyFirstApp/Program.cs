4d33ef1c60c8c8e13d9047a9a725b83ca64c2bad
Rainer Targama <rainertargama@gmail.com>
2024-02-12 20:32:53 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, I am Joe Biden!");
+Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+//string interp $ {}
+Console.WriteLine($"Hello, {username}!");
